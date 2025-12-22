8c6c502f26128a23833a0a660c9f82f69ddf8412
santu7212 <santupramanik540@gmail.com>
2025-12-22 19:17:54 +0530

    feat: add Features, Title, and Testimonial components to enhance Home page layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../components/home/Banner";
+import Features from "../components/home/Features";
 import Hero from "../components/home/Hero";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner />
       <Hero/>
+      <Features/>
     </div>
   );
 };
