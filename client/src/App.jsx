f3e3983b040b1a8e418b7bd14b8a1271f3bae2c2
santu7212 <santupramanik540@gmail.com>
2025-12-22 13:27:02 +0530

    feat: set up routing with React Router and define main application structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     <div>
-       
-     </div>
-   )
- }
- 
- export default App
- 
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashbord from "./pages/Dashbord";
+import Layout from "./pages/Layout";
+import Preview from "./pages/Preview";
+import ResumeBuilder from "./pages/ResumeBuilder";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/dashboard", element: <Dashbord /> },
+      { path: "/layout", element: <Layout /> },
+      { path: "/preview", element: <Preview /> },
+      { path: "/resume-builder", element: <ResumeBuilder /> },
+    ],
+  },
+]);
+
+const App = () => {
+  return  <RouterProvider router={router}/>
+};
+
+export default App;
