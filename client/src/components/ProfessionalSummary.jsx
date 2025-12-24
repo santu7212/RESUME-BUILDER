f35bf4403a31513c0185d41a3e324b11c95e5bb4
santu7212 <santupramanik540@gmail.com>
2025-12-24 19:25:44 +0530

    feat: enhance ProfessionalSummary component layout; add AI Enhance button

--- client/src/components/ProfessionalSummary.jsx
+++ client/src/components/ProfessionalSummary.jsx
@@ -1,8 +1,26 @@
+import { Sparkle } from 'lucide-react'
 import React from 'react'
 
 const ProfessionalSummary = ({data,onChange,setResumeData}) => {
   return (
-    <div>
+    <div className='space-y-4'>
+        <div className='flex items-center justify-between'> 
+            <div>
+                <h3 className='flex items-center gap-2 text-lg
+                font-semibold text-gray-900'>Professional Summary</h3>
+                <p className='text-sm text-gray-500'>
+                    Add summary for your resume here
+                </p>
+
+            </div>
+            <button  className="flex items-center gap-2 px-3 py-1 text-sm bg-purple-100
+             text-purple-700 rounded hover:bg-purple-200
+             transition-colors disabled:opacity-50">
+                <Sparkle className='size-4'/>
+                AI Enhance
+            </button>
+
+        </div>
       
     </div>
   )
