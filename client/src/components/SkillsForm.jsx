46a27550fba8c22542a3e0686a7efddf8fcaa220
santu7212 <santupramanik540@gmail.com>
2025-12-25 13:07:27 +0530

    function to remove skill

--- client/src/components/SkillsForm.jsx
+++ client/src/components/SkillsForm.jsx
@@ -1,3 +1,4 @@
+import { Newspaper, Plus, X } from "lucide-react";
 import React, { useState } from "react";
 
 const SkillsForm = ({ data, onChange }) => {
@@ -19,7 +20,65 @@ const SkillsForm = ({ data, onChange }) => {
       addSkill();
     }
   };
-  return <div></div>;
+  return (
+    <div className="space-y-4">
+      <div>
+        <h3
+          className="flex items-center gap-2 text-lg font-semibold
+        text-gray-900"
+        >
+          Skills
+        </h3>
+        <p className="text-sm text-gray-500">
+          Add your technical and soft skills
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Enter Your skills"
+          className="flex-1 px-3 py-2 text-sm"
+          onChange={(e) => setNewSkill(e.target.value)}
+          value={newSkill}
+          onKeyDown={handleKeyPress}
+        />
+        <button
+          onClick={addSkill}
+          disabled={!newSkill.trim}
+          className="flex items-center gap-2 px-4 py-2 text-sm
+             bg-blue-600 text-white rounded-lg
+             hover:bg-blue-700 transition-colors
+             disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          <Plus size={4} /> Add
+        </button>
+      </div>
+      {data.length>0 ? (
+        <div className="flex flex-wrap gap-2">
+            {data.map((skill,index)=>(
+                <span key={index} className="flex items-center gap-1 px-3 py-1
+                bg-blue-100 text-blue-800 rounded-full text-sm">
+                    {skill}
+                    <button
+                    onClick={()=>removeSkill(index)}
+                     className="ml-1 hover:bg-blue-200 rounded-full p-0.5 
+                    transition-colors">
+                        <X className="w-3 h-3"/>
+
+                    </button>
+
+                </span>
+
+            ))}
+
+        </div>
+      ):(
+        <div>
+
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SkillsForm;
