8b23c79acf88e47afc14596baa97dc5f0c295f6f
santu7212 <santupramanik540@gmail.com>
2025-12-25 13:38:25 +0530

    add share private and download buttons

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -6,9 +6,14 @@ import {
   Briefcase,
   ChevronLeft,
   ChevronRight,
+  Download,
+  DownloadIcon,
+  EyeIcon,
+  EyeOffIcon,
   FileText,
   FolderIcon,
   GraduationCap,
+  Share2Icon,
   Sparkle,
   User,
 } from "lucide-react";
@@ -189,8 +194,39 @@ const ResumeBuilder = () => {
 
           {/* Right pannel-previw */}
           <div className="lg:col-span-7 max-lg:mt-6">
-            <div>
-              {/* __buttons */}
+            <div className="relative w-full">
+              <div className="absolute bottom-3 left-0 right-0 flex items-center 
+              justify-end gap-2">
+                {resumeData.public &&(
+                  <button  
+                    className="flex items-center p-2 px-4 gap-2 text-xs
+             bg-gradient-to-br from-blue-100 to-blue-200
+             text-blue-600 rounded-lg ring-blue-300
+             hover:ring transition-colors">
+                    <Share2Icon className="size-4"/>
+                  </button>
+                )}
+                <button 
+                 className="flex items-center p-2 px-4 gap-2 text-xs
+             bg-gradient-to-br from-purple-100 to-purple-200
+             text-purple-600 ring-purple-300 rounded-lg
+             hover:ring transition-colors"
+                >
+                  {resumeData.public ? <EyeIcon className="size-4"/>:
+                  <EyeOffIcon className="size-4"/>}
+                  {resumeData.public?"public" :"private"}
+                </button>
+
+                <button
+                 className="flex items-center gap-2 px-6 py-2 text-xs
+             bg-gradient-to-br from-blue-100 to-blue-200
+             text-blue-600 rounded-lg ring-blue-300
+             hover:ring transition-colors" 
+                >
+                  <DownloadIcon className="size-4"/>Download
+                </button>
+
+              </div>
             </div>
             {/* Resume preview */}
             <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
