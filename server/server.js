1dc095fcd574c6b73800876f379aacbb489c674f
santu7212 <santupramanik540@gmail.com>
2025-12-25 19:50:45 +0530

    create a server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+const app=express()
+
+const PORT=process.env.PORT || 3001;
+
+app.use(express.json())
+app.use(cors())
+app.get("/",(req,res)=>res.send("Server is live..."))
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`)
+)
