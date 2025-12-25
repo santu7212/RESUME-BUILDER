d44b800d0e014ea8871d17c3ec8d894b978b5617
santu7212 <santupramanik540@gmail.com>
2025-12-25 12:18:52 +0530

    feat: enhance ProjectForm component; add project management UI with add and remove functionality

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,3 +1,4 @@
+import { Plus, Trash2 } from 'lucide-react';
 import React from 'react'
 
 const ProjectForm = ({data,onChange}) => {
@@ -22,6 +23,96 @@ const ProjectForm = ({data,onChange}) => {
   }
   return (
     <div>
+          <div className="flex items-center justify-between">
+        <div >
+          <h3
+            className="flex items-center gap-2 text-lg
+                font-semibold text-gray-900"
+          >
+            Projects
+          </h3>
+          <p className="text-sm text-gray-500">
+            Add your projects
+          </p>
+        </div>
+        <button onClick={addProject}
+          className="flex items-center gap-2 px-3 py-1 text-sm bg-green-100
+             text-green-700 rounded-lg hover:bg-green-200
+             transition-colors "
+        >
+          <Plus className="size-4" />
+        Add Projects
+        </button>
+      </div>
+
+    
+       
+        <div className='space-y-4 mt-6'>
+          {data.map((project,index)=>(
+
+            <div key={index} className='p-4 border border-gray-200
+            rounded-lg space-y-3'>
+              <div className='flex justify-between items-start'>
+                <h4>Education #{index+1}</h4>
+                <button onClick={()=>removeProject(index)}
+                 className='text-red-500 hover:text-red-700
+                transition-colors'>
+                  <Trash2 className='size-4'/>
+                </button>
+
+              </div>
+              <div className='grid md:grid-cols-2 gap-3'>
+                <input onChange={(e)=>updateProject(index,"name",e.target.value)}
+                 value={project.name || ""}
+                type="text" placeholder='Project Name '
+                className='px-3 py-2 text-sm  rounded-lg' />
+
+
+
+                 <input onChange={(e)=>updateProject(index,"type",e.target.value)}
+                 value={project.type || ""}
+                type="text" placeholder='Project Type'
+                className='px-3 py-2 text-sm  rounded-lg' />
+
+
+                
+                 <textarea rows={4}
+                  onChange={(e)=>updateProject(index,"description",e.target.value)}
+                 value={project.description || ""}
+                type="text" placeholder='Describe your project...'
+                className='w-full  px-3 py-2 text-sm  rounded-lg resize-none' />
+
+
+
+
+
+                
+
+              
+
+
+
+
+
+
+              </div>
+
+
+               
+
+
+              
+              
+              
+              
+
+
+               
+            </div>
+          ))}
+
+        </div>
+    
       
     </div>
   )
