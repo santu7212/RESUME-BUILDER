5a0205e0cd1aa065ae3a2de094b9ccd6aecce647
santu7212 <santupramanik540@gmail.com>
2025-12-26 18:18:02 +0530

    create schema for user

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import connectDB from "./config/db.js";
 
-const app=express()
+const app = express();
 
-const PORT=process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 // DB connection
-await connectDB()
+await connectDB();
 
-app.use(express.json())
-app.use(cors())
-app.get("/",(req,res)=>res.send("Server is live..."))
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`)
-)
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => res.send("Server is live..."));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
