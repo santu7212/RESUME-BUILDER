dddafec3cc9e8ce253c143f3ff6dc9274dc6ade7
santu7212 <santupramanik540@gmail.com>
2025-12-26 19:31:11 +0530

    controller funtion for login user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -26,7 +26,7 @@ const registerUser = async (req, res) => {
       email,
       password,
     });
-    const token =generateToken(user._id)
+    const token = generateToken(user._id);
 
     user.password = undefined;
 
@@ -44,5 +44,34 @@ const registerUser = async (req, res) => {
     });
   }
 };
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Enter all details" });
+    }
 
-export default registerUser;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json("User with this email does not exist!! Register first");
+    }
+    if (!user.comparePassword(password)) {
+      return res.status(400).json("Invalid password or email");
+    }
+    const token = generateToken(user._id);
+    user.password = undefined;
+    return res
+      .status(201)
+      .json({ success: true, message: "Login successfull", token, user });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export { registerUser, loginUser };
