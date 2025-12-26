21832bb7e547e7c3af1839fafb190beac7996188
santu7212 <santupramanik540@gmail.com>
2025-12-26 19:00:31 +0530

    function for register user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,19 +1,38 @@
 import User from "../models/user.model.js";
 
-
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
       return res.status(400).json({ success: true, message: "Missing fields" });
     }
-    const user = await User.findOne({ email });
-    if (user) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
       return res
         .status(400)
         .json({ success: true, message: "User with this email already exist" });
     }
 
-    
-  } catch (error) {}
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    user.passsword = undefined;
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error("Register error:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 };
+
+export default registerUser;
