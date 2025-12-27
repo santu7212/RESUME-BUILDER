ebde6d20faaccaafa9bab7b73b3129ee30293a7e
santu7212 <santupramanik540@gmail.com>
2025-12-27 12:10:42 +0530

    endpoint for get resume

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getUser,
+  getUserResume,
   loginUser,
   registerUser,
 } from "../controllers/user.controller.js";
@@ -11,5 +12,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/user-data", protect, getUser);
+userRouter.get("/resume", protect, getUserResume);
 
 export default userRouter;
