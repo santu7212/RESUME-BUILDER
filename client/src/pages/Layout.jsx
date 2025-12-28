8e22407627d4d1fd1489dd87eb8e6656dceb6b26
santu7212 <santupramanik540@gmail.com>
2025-12-28 11:11:42 +0530

    import login page in layout

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,6 +3,7 @@ import { NavLink, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import Loader from "../components/Loader";
+import Login from "./Login";
 
 const Layout = () => {
   const { user, loading } = useSelector((state) => state.auth);
