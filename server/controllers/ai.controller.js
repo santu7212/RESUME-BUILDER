f35757e30009c4d6106063663ab5ca109f095f8f
santu7212 <santupramanik540@gmail.com>
2025-12-28 09:42:04 +0530

    controller function to enhance job description

--- server/controllers/ai.controller.js
+++ server/controllers/ai.controller.js
@@ -32,4 +32,36 @@ const enhanceProfessionalSummary = async (req, res) => {
   }
 };
 
-export { enhanceProfessionalSummary };
+const enhanceJobDescription = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing required fields" });
+    }
+
+    const response = await ai.chat.completions.create({
+      model: process.env.OPENAI_MODEL,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else.",
+        },
+
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+    });
+
+    const enhancedContent = response.choices[0].message.content;
+    return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+export { enhanceProfessionalSummary, enhanceJobDescription };
