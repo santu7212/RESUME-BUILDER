9ee43ccdf73cf6cfdaf6f194472852694fc352ab
santu7212 <santupramanik540@gmail.com>
2025-12-28 11:29:18 +0530

    configure api axios and get user data through axious and get token from local storage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,40 +7,51 @@ import Layout from "./pages/Layout";
 import Preview from "./pages/Preview";
 import ResumeBuilder from "./pages/ResumeBuilder";
 import { useDispatch } from "react-redux";
+import api from "./configs/api.js";
+import { login, setLoading } from "./app/features/authSlice.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     children: [
-     
       {
         path: "app",
-        element: <Layout/>,
+        element: <Layout />,
         children: [
-          { index: true, element: <Dashboard/> },
+          { index: true, element: <Dashboard /> },
           { path: "resume-builder/:resumeID", element: <ResumeBuilder /> },
         ],
       },
     ],
   },
   { path: "view/:resumeID", element: <Preview /> },
- 
 ]);
 
 const App = () => {
-  const dispatch=useDispatch()
-  const getUserData=async()=>{
-    const token=localStorage.getItem("token")
+  const dispatch = useDispatch();
+  const getUserData = async () => {
+    const token = localStorage.getItem("token");
     try {
-      if(token){
-        const {data}=await 
+      if (token) {
+        const { data } = await api.get("/api/user/user-data", {
+          headers: { Authorization: token },
+        });
+        if (data.user) {
+          dispatch(login({ token, user: data.user }));
+        }
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
       }
-      
     } catch (error) {
-      
+      dispatch(setLoading(false));
+      console.log(error.message);
     }
-  }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
   return <RouterProvider router={router} />;
 };
 
