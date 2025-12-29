e2bc0021252cc1c93958abf9a7b54aabf50507b9
santu7212 <santupramanik540@gmail.com>
2025-12-29 11:30:40 +0530

    implement the function to enhnace professional summary

--- client/src/components/ProfessionalSummary.jsx
+++ client/src/components/ProfessionalSummary.jsx
@@ -1,7 +1,34 @@
-import { Sparkle } from "lucide-react";
-import React from "react";
+import { Loader2, Sparkle } from "lucide-react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import api from "../configs/api";
+import toast from "react-hot-toast";
 
 const ProfessionalSummary = ({ data, onChange, setResumeData }) => {
+  const { token } = useSelector((state) => state.auth);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const generateSummary = async () => {
+    try {
+      setIsGenerating(true);
+      const prompt = `enhance my professional summary "${data}"`;
+      const response = await api.post(
+        "/api/ai/enhance-pro-sum",
+        { userContent: prompt },
+        { headers: { Authorization: token } }
+      );
+      console.log("Enhanced:", response.data.enhancedContent);
+// console.log("Resume data:", resumeData);
+
+      setResumeData((prev) => ({
+        ...prev,
+        professional_summary: response.data.enhancedContent,
+      }));
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    } finally {
+      setIsGenerating(false);
+    }
+  };
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
@@ -17,28 +44,37 @@ const ProfessionalSummary = ({ data, onChange, setResumeData }) => {
           </p>
         </div>
         <button
+          disabled={isGenerating}
+          onClick={generateSummary}
           className="flex items-center gap-2 px-3 py-1 text-sm bg-purple-100
              text-purple-700 rounded hover:bg-purple-200
              transition-colors disabled:opacity-50"
         >
-          <Sparkle className="size-4" />
-          AI Enhance
+          {isGenerating ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            <Sparkle className="size-4" />
+          )}
+          {isGenerating? "Enhancing..." : "AI Enhance"}
+          
         </button>
       </div>
       <div className="mt-6">
         <textarea
-        placeholder="write summary"
-        value={data || ""}
-        onChange={(e)=>onChange(e.target.value)}
-           rows={7}
+          placeholder="write summary"
+          value={data || ""}
+          onChange={(e) => onChange(e.target.value)}
+          rows={7}
           className="w-full p-3 px-4 mt-2 border text-sm
              border-gray-300 rounded-lg
              focus:ring focus:ring-blue-500
              focus:border-blue-500 outline-none
              transition-colors resize-none"
-        /> 
-        <p className="text-xs text-gray-500 max-w-4/5 mx-auto text-center"
-        >Tip: keep it concise(3-4 sentences) and focus on your most relevant achievement and skills.</p>
+        />
+        <p className="text-xs text-gray-500 max-w-4/5 mx-auto text-center">
+          Tip: keep it concise(3-4 sentences) and focus on your most relevant
+          achievement and skills.
+        </p>
       </div>
     </div>
   );
